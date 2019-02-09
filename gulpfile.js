8cb512c616aacd04903dc65178fd77259a6e267b
Kevin Niemeyer <niemeyer2012@gmail.com>
2019-02-09 06:40:20 -0600

    Completed resume project

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp'),
-  	connect = require('gulp-connect');
-  	
+  	connect = require('gulp-connect'),
+  	cache = require('gulp-cache');
 
 gulp.task('connect', async function() {
   connect.server();
+
 });
 
 gulp.task('default', gulp.series('connect'));
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+
+
 //function to extract the id out of the URL
 function getParameterByName( name ){
   name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
@@ -13,10 +15,12 @@ function getParameterByName( name ){
 
 //helpers are best for formatting data
 //expression helpers
-Handlebars.registerHelper("formatName", function(property1) {
-	//helpers always retur a string
-	//argument is always a property from your data
-	return new Handlebars.SafeString("<strong>Name: </strong>" + property1);
+//helpers always returns a string
+//argument is always a property from your data
+Handlebars.registerHelper("capitalizeWords", function(property1) {
+//capitalize all words.	
+	property1 = property1.replace(/\b\w/g, l => l.toUpperCase())
+	return new Handlebars.SafeString("<strong>" + property1 + "</strong>" );
 });
 
 //expression helper
@@ -36,40 +40,31 @@ Handlebars.registerHelper("toLower", function(options) {
 	return options.fn(this).toLowerCase();
 });
 
+Handlebars.registerHelper('equals', function(first, second, options) {
+if (first == second) {
+return options.fn(this);
+}
+});
+
+Handlebars.registerPartial("resumeApplicantPartial", $("#resume-applicant-partial").html());
+
+Handlebars.registerPartial("resumeExperiencePartial", $("#resume-experience-partial").html());
+
+Handlebars.registerPartial("resumeEducationPartial", $("#resume-education-partial").html());
+
+Handlebars.registerPartial("resumeReferencesPartial", $("#resume-references-partial").html());
+
+
 $(document).ready(function() {
-	console.log(window.location.href);
 	var resumeTemplate = $("#resume-template").html();
 
 	var compiledResumeTemplate = Handlebars.compile(resumeTemplate);
 	
-	var jobId = getParameterByName("id");
-	
-	$.ajax("./job-duties-partial.html").done(function(jobDutiesPartial) {
-		console.log(jobDutiesPartial)
-		//can't get jobDutiesPartial to not be undefined
-
-		$("body").append(jobDutiesPartial);
-		//have to register the partial before you append it to the body:
-		Handlebars.registerPartial("jobDutiesPartial", $("#job-duties-partial").html());
-		
-		console.log("after partial");
-	});
 
 	$.ajax("./data/resume.json").done(function(resume) {
-		if($("body").hasClass("page-job-duties")) {
-			
-			$(".resume-container").html(compiledResumeTemplate(resume.experience[jobId]));
-		} else {
-			$(".resume-container").html(compiledResumeTemplate(resume));
-		}
-
 		
+			console.log("test");
+			$(".resume-container").html(compiledResumeTemplate(resume));		
 	});
-
-	$(".resume-container").on("click", ".view-details", function(e) {
-		//e.preventDefault(); //anchor, so don't want to jump
-	});
-	// **you're trying to add an event listener to the view-details button before it's on the page; you could put it inside of the ajax call to quick fix that; this compiles the template first, THEN adds the event listener, but that clutters up the ajax. a better way is to use an even delegate that says if the document is clicked, don't do anything unless it is the button that was clicked. instead of using document, you can use something that is closer to the button, but before the template, like the ul resume-container tag.
-
 });
 
